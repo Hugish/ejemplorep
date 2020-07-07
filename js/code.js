8473889ac52d3519a5ec2dba7443cdf4df3894d6
Hugish <hugish04@gmail.com>
2020-07-07 17:58:50 -0600

    initial commit

---
+++ js/code.js
@@ -0,0 +1,260 @@
+
+
+
+$( "#repetir" ).click(function() {
+ $(".input").val("");
+
+});
+
+
+
+$( "#check" ).click(function() {
+  //alert( "Hola mundo" );
+	
+ var dato1=$( "#dato1" ).val(); 
+ 	dato2= $( "#dato2" ).val();
+ 	dato3=$( "#dato3" ).val();  
+ 	dato4=$( "#dato4" ).val();
+  dato5=$( "#dato5" ).val();
+  dato6=$( "#dato6" ).val();
+  dato7=$( "#dato7" ).val();
+  dato8=$( "#dato8" ).val();
+  dato9=$( "#dato9" ).val();
+  dato10=$( "#dato10" ).val();
+  dato11=$( "#dato11" ).val();
+  dato12=$( "#dato12" ).val();
+  dato13=$( "#dato13" ).val();
+  dato14=$( "#dato14" ).val();
+  dato15= $( "#dato15" ).val();
+  dato16= $( "#dato16" ).val();
+  dato17= $( "#dato17" ).val();
+  dato18= $( "#dato18" ).val();
+  dato19= $( "#dato19" ).val();
+  dato20= $( "#dato20" ).val();
+  dato21= $( "#dato21" ).val();
+  dato22= $( "#dato22" ).val();
+  dato23= $( "#dato23" ).val();
+  dato24= $( "#dato24" ).val();
+  dato25= $( "#dato25" ).val();
+  dato26= $( "#dato26" ).val();
+  dato27= $( "#dato27" ).val();
+  dato28= $( "#dato28" ).val();
+  dato29= $( "#dato29" ).val();
+  dato30= $( "#dato30" ).val();
+  dato31= $( "#dato31" ).val();
+  dato32= $( "#dato32" ).val();
+  dato33= $( "#dato33" ).val();
+  dato34= $( "#dato34" ).val();
+  dato35= $( "#dato35" ).val();
+  dato36= $( "#dato36" ).val();
+  dato37= $( "#dato37" ).val();
+  dato38= $( "#dato38" ).val();
+  dato39= $( "#dato39" ).val();
+
+
+ 	if (dato1=="1") {
+     $( "#dato1" ).css("background-color","#7590AB");
+  	 $( "#dato1" ).val(); 
+  } else{ $( "#dato1" ).css("background-color","red");
+    
+}
+
+  if (dato2=="6") {
+    $( "#dato2" ).css("background-color","#7590AB");
+  	$( "#dato2" ).val(); 
+  } else{$( "#dato2" ).css("background-color","red");
+  }
+
+   if (dato3=="9") {
+  	$( "#dato3" ).css("background-color","#7590AB");
+  	$( "#dato3" ).val(); 
+  } else{$( "#dato3" ).css("background-color","red");}
+
+  if (dato4=="4") {
+  	$( "#dato4" ).css("background-color","#7590AB");
+  	$( "#dato4" ).val(); 
+  } else{$( "#dato4" ).css("background-color","red");
+  }
+
+  if (dato5=="2") {
+    $( "#dato5" ).css("background-color","#7590AB");
+    $( "#dato5" ).val(); 
+  } else{$( "#dato5" ).css("background-color","red");}
+
+  if (dato6=="4") {
+    $( "#dato6" ).css("background-color","#7590AB");
+    $( "#dato6" ).val(); 
+  } else{$( "#dato6" ).css("background-color","red");}
+
+  if (dato7=="1") {
+    $( "#dato7" ).css("background-color","#7590AB");
+    $( "#dato7" ).val(); 
+  } else{$( "#dato7" ).css("background-color","red");}
+
+  if (dato8=="5") {
+    $( "#dato8" ).css("background-color","#7590AB");
+    $( "#dato8" ).val(); 
+  } else{$( "#dato8" ).css("background-color","red");}
+
+  if (dato9=="3") {
+    $( "#dato9" ).css("background-color","#7590AB");
+    $( "#dato9" ).val(); 
+  } else{$( "#dato9" ).css("background-color","red");}
+
+  if (dato10=="3") {
+    $( "#dato10" ).css("background-color","#7590AB");
+    $( "#dato10" ).val(); 
+  } else{$( "#dato10" ).css("background-color","red");}
+
+  if (dato11=="9") {
+    $( "#dato11" ).css("background-color","#7590AB");
+    $( "#dato11" ).val(); 
+  } else{$( "#dato11" ).css("background-color","red");}
+
+  if (dato12=="5") {
+    $( "#dato12" ).css("background-color","#7590AB");
+    $( "#dato12" ).val(); 
+  } else{$( "#dato12" ).css("background-color","red");}
+
+  if (dato13=="7") {
+    $( "#dato13" ).css("background-color","#7590AB");
+    $( "#dato13" ).val(); 
+  } else{$( "#dato13" ).css("background-color","red");}
+
+  if (dato14=="1") {
+    $( "#dato14" ).css("background-color","#7590AB");
+    $( "#dato14" ).val(); 
+  } else{$( "#dato14" ).css("background-color","red");}
+
+   if (dato15=="9") {
+    $( "#dato15" ).css("background-color","#7590AB");
+    $( "#dato15" ).val(); 
+  } else{$( "#dato15" ).css("background-color","red");}
+
+   if (dato16=="3") {
+    $( "#dato16" ).css("background-color","#7590AB");
+    $( "#dato16" ).val(); 
+  } else{$( "#dato16" ).css("background-color","red");}
+
+ if (dato17=="7") {
+    $( "#dato17" ).css("background-color","#7590AB");
+    $( "#dato17" ).val(); 
+  } else{$( "#dato17" ).css("background-color","red");}
+
+ if (dato18=="2") {
+    $( "#dato18" ).css("background-color","#7590AB");
+    $( "#dato18" ).val(); 
+  } else{$( "#dato18" ).css("background-color","red");}
+
+ if (dato19=="8") {
+    $( "#dato19" ).css("background-color","#7590AB");
+    $( "#dato19" ).val(); 
+  } else{$( "#dato19" ).css("background-color","red");}
+
+ if (dato20=="3") {
+    $( "#dato20" ).css("background-color","#7590AB");
+    $( "#dato20" ).val(); 
+  } else{$( "#dato20" ).css("background-color","red");}
+
+  if (dato21=="5") {
+    $( "#dato21" ).css("background-color","#7590AB");
+    $( "#dato21" ).val(); 
+  } else{$( "#dato21" ).css("background-color","red");}
+
+  if (dato22=="4") {
+    $( "#dato22" ).css("background-color","#7590AB");
+    $( "#dato22" ).val(); 
+  } else{$( "#dato22" ).css("background-color","red");}
+
+   if (dato23=="6") {
+    $( "#dato23" ).css("background-color","#7590AB");
+    $( "#dato23" ).val(); 
+  } else{$( "#dato23" ).css("background-color","red");}
+
+ if (dato24=="8") {
+    $( "#dato24" ).css("background-color","#7590AB");
+    $( "#dato24" ).val(); 
+  } else{$( "#dato24" ).css("background-color","red");}
+
+  if (dato25=="1") {
+    $( "#dato25" ).css("background-color","#7590AB");
+    $( "#dato25" ).val(); 
+  } else{$( "#dato25" ).css("background-color","red");}
+
+  if (dato26=="9") {
+    $( "#dato26" ).css("background-color","#7590AB");
+    $( "#dato26" ).val(); 
+  } else{$( "#dato26" ).css("background-color","red");}
+
+  if (dato27=="5") {
+    $( "#dato27" ).css("background-color","#7590AB");
+    $( "#dato27" ).val(); 
+  } else{$( "#dato27" ).css("background-color","red");}
+
+
+if (dato28=="8") {
+    $( "#dato28" ).css("background-color","#7590AB");
+    $( "#dato28" ).val(); 
+  } else{$( "#dato28" ).css("background-color","red");}
+
+if (dato29=="4") {
+    $( "#dato29" ).css("background-color","#7590AB");
+    $( "#dato29" ).val(); 
+  } else{$( "#dato29" ).css("background-color","red");}
+
+  if (dato30=="6") {
+    $( "#dato30" ).css("background-color","#7590AB");
+    $( "#dato30" ).val(); 
+  } else{$( "#dato30" ).css("background-color","red");}
+
+  if (dato31=="5") {
+    $( "#dato31" ).css("background-color","#7590AB");
+    $( "#dato31" ).val(); 
+  } else{$( "#dato31" ).css("background-color","red");}
+
+   if (dato32=="5") {
+    $( "#dato32" ).css("background-color","#7590AB");
+    $( "#dato32" ).val(); 
+  } else{$( "#dato32" ).css("background-color","red");}
+
+   if (dato33=="3") {
+    $( "#dato33" ).css("background-color","#7590AB");
+    $( "#dato33" ).val(); 
+  } else{$( "#dato33" ).css("background-color","red");}
+
+   if (dato34=="7") {
+    $( "#dato34" ).css("background-color","#7590AB");
+    $( "#dato34" ).val(); 
+  } else{$( "#dato34" ).css("background-color","red");}
+
+
+   if (dato35=="8") {
+    $( "#dato35" ).css("background-color","#7590AB");
+    $( "#dato35" ).val(); 
+  } else{$( "#dato35" ).css("background-color","red");}
+
+   if (dato36=="6") {
+    $( "#dato36" ).css("background-color","#7590AB");
+    $( "#dato36" ).val(); 
+  } else{$( "#dato36" ).css("background-color","red");}
+
+   if (dato37=="1") {
+    $( "#dato37" ).css("background-color","#7590AB");
+    $( "#dato37" ).val(); 
+  } else{$( "#dato37" ).css("background-color","red");}
+
+   if (dato38=="2") {
+    $( "#dato38" ).css("background-color","#7590AB");
+    $( "#dato38" ).val(); 
+  } else{$( "#dato38" ).css("background-color","red");}
+
+   if (dato39=="4") {
+    $( "#dato39" ).css("background-color","#7590AB");
+    $( "#dato39" ).val(); 
+  } else{$( "#dato39" ).css("background-color","red");}
+  
+
+
+});
+
+
